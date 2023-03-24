115d99390edf512be2c18fba2f785667cf1990e8
Tobiasz Tonn <toneq@tlen.pl>
2023-03-24 08:27:06 +0100

    poprawa migracji - usunięcie spacji z nazwy

--- database/migrations/2023_03_24_070239_create_currency_table.php
+++ database/migrations/2023_03_24_070239_create_currency_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\QueryException;
 
 class CreateCurrencyTable extends Migration
 {
@@ -14,9 +15,9 @@ class CreateCurrencyTable extends Migration
     public function up()
     {
         Schema::create('currency', function (Blueprint $table) {
-            $table->uuid('id')->default(DB::raw('(UUID())'));
+            $table->uuid('id')->primary();
             $table->string('name', '50');
-            $table->string('currency_code ', '10');
+            $table->string('currency_code', '10');
             $table->decimal('exchange_rate',10,2);
             $table->timestamps();
         });
