b8c5bf32d56fad0e3542c5f84460bf2b6f65667f
Tobiasz Tonn <toneq@tlen.pl>
2023-03-24 08:22:20 +0100

    Pierwszy kod do testów

--- app/Http/Controllers/Api/CurrencyController.php
+++ app/Http/Controllers/Api/CurrencyController.php
@@ -5,59 +5,47 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Currency;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class CurrencyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Currency  $currency
-     * @return \Illuminate\Http\Response
-     */
     public function show(Currency $currency)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Currency  $currency
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Currency $currency)
     {
-        //
+        $client = new Client();
+
+        $response = $client->request('GET', 'http://api.nbp.pl/api/exchangerates/tables/A');
+
+        $data = json_decode($response->getBody(), true);
+
+        $date = $data[0]['effectiveDate'];
+
+        foreach ($data[0]['rates'] as $rate) {
+            //Początkowy test czy zapisuje wszystkie
+            $currency = new Currency;
+            $currency->name = $rate['Currency'];
+            $currency->currency_code = $rate['code'];
+            $currency->exchange_rate = $rate['mid'];
+            $currency->save();
+        }
+
+        return 'Kursy walut zostały zaktualizowane.';
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Currency  $currency
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Currency $currency)
     {
         //
